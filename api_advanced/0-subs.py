f663d9ba39bab438cfc6cf7b76bf23c3b098c64d
Cshoga <116923054+Cshoga@users.noreply.github.com>
2024-07-05 10:12:25 -0700

    commit all

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """
-A script that use reddit api to outputs the
+A script that uses the Reddit API to output the
 number of subscribers of a certain subreddit.
 """
-
 
 import json
 import requests
 
-
 def number_of_subscribers(subreddit):
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raises exception for bad responses (4xx or 5xx)
 
-    if response.status_code == 200:
-        data = json.loads(response.text)
+        data = response.json()
         return data['data']['subscribers']
 
-    else:
+    except requests.RequestException as e:
+        print(f"Error fetching subreddit data: {e}")
         return 0
